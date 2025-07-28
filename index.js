56938d7ab337887efa1565a6d20a5c29dbf8b64e
Van Vo <hongvant2@gmail.com>
2025-07-28 16:15:45 +1200

    Remove duplicate dotenv configuration in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
 const productRoutes = require('./models/product');
 const authRoutes = require('./routes/auth');
 const messageRoutes = require('./routes/message');
