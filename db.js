f1ffcaafc42cfa833d92579ac79b36fc205c66a6
Van Vo <hongvant2@gmail.com>
2025-07-30 14:23:19 +1200

    Remove dotenv configuration from db.js and ensure SSL settings are properly defined

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -7,6 +6,12 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false // Required for DO
+  },
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
 module.exports = pool;
